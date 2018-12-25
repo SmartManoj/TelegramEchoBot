8c5c855c9e583f844968fb6317ab2ad9a5d9cbd3
SmartManoj <smartmanoj42857@gmail.com>
2018-12-25 14:48:50 +0530

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -16,7 +16,19 @@ print('Started')
 @app.route('/')
 def main():
     return f'''<html><head><title>Telegram Echobot</title></head><body>Telegram Echobot</body></html>'''
-
+def alink(s,k=None):
+    if not k:k=s
+    return '<a href="{}">{}</a>'.format(s,k)
+def slink(s,k=None,v=1):
+    ss=s
+    for i,j in (('__','\n'),('--',' '),('-','.'),('_','@'),):
+            s=s.replace(j,i)
+    if k:ss=k
+    s=f'https://t.me/smartmanojbot?start={s}'
+    return '<a href="{}">{}</a>'.format(s,ss) if v else s
+def tclink(s,d=None):
+    if d:return f"+91{s[-10:]} | {slink(f'w.{s}',d)} |"
+    return slink(f'w.{s}',f"+91{s[-10:]}")
 def get_updates(offset=None):
     url = URL + "getUpdates?timeout=100"
     if offset:
@@ -31,7 +43,7 @@ def get_last_update_id(updates):
 
 def send_message(text, chat_id):
     text = urllib.parse.quote_plus(text)
-    url=URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
+    url=URL + "sendMessage?text={}&chat_id={}&parse_mode=html".format(text, chat_id)
     try:
         r=requests.get(url,timeout=30)
         if r.reason!='OK':print(r.text)
@@ -56,14 +68,35 @@ def main():
 def echo_all(updates):
     for update in updates["result"]:
         try:
-            print(update)
+            fname =update["message"]['chat']['first_name']
             chat = update["message"]["chat"]["id"]
-            a = update["message"].get("text")
-            #customize here
-            if a: print(chat,a)
-            send_message(a,chat)
+            text = update["message"].get("text")
+            msg(text,fname,chat)
         except Exception as e:
             print(e)
+def msg(text,fname='',chat=admin):
+    if text:
+        print(chat,text)
+    text=text.lower()
+    text=text.replace('-','')
+    text=text.strip('/')
+    if text.startswith('start'):
+        v=text.split()
+        if len(v)==1:
+            text='Welcome {}'.format(fname)
+            send_message(text,chat)
+            text='/help'
+        else:
+            msgf(v[1],chat,name=name);return
+    elif text.startswith('help'):
+        h='''
+/help
+'''
+        text=(h)
+    elif text.startswith('hi'):text='Hi buddy'
+            #customize here
+    if text: print(chat,text)
+    send_message(text,chat)
 
 def snt(f,a,b=None):
   try:
